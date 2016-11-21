36c6476c77b291735a9d58ea0a27b6701d697140
cmcampione <cmcampione@gmail.com>
2016-11-21 16:05:00 +0100

    Release version 1.0.18

--- release.bat
+++ release.bat
@@ -3,9 +3,8 @@ if "%1"=="" goto blank
 
 ECHO "Releasing version %1 ..."
 
-@echo on
+call gulp
 
-gulp
 git commit -am "Release version %1"
 git tag -a v%1 -m "Release version %1"
 git push origin master --tags
