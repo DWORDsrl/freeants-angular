e869defa9b9514bc8e4c8575db7339b74fc38309
Andrea Giuffrida <Andrea Giuffrida>
2016-11-25 15:37:56 +0100

    Update Scheduler Datacontext

--- src/scheduler.datacontext.js
+++ src/scheduler.datacontext.js
@@ -11,10 +11,32 @@
     return {
         schedule: function (schedulerObject) {
             var req = $http({
-                method: 'PUT',
+                method: 'POST',
                 headers: helpers.getSecurityHeaders(),
                 url: SchedulerRegisterUrl(),
                 data: schedulerObject
+            }).then(function (response) {
+                return response.data;
+            });
+            return req;
+        },
+        update: function (schedulerObject, schedulerId) {
+            var req = $http({
+                method: 'PUT',
+                headers: helpers.getSecurityHeaders(),
+                url: SchedulerRegisterUrl() + "/" + schedulerId,
+                data: schedulerObject
+            }).then(function (response) {
+                return response.data;
+            });
+            return req;
+        },
+         delete: function (schedulerId) {
+            var req = $http({
+                method: 'DELETE',
+                headers: helpers.getSecurityHeaders(),
+                url: SchedulerRegisterUrl() + "/" + schedulerId,
+                data: null
             }).then(function (response) {
                 return response.data;
             });
