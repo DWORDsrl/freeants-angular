2822b6b8babacf3c6757b0d34a997bd1282bcbb2
cmcampione <cmcampione@gmail.com>
2017-01-02 19:32:13 +0100

    Release version 1.0.79

---
+++ src/imagesRawDatacontext.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    angular.module('freeants').factory('imagesRawDataContext', ['$http', 'helpers','path', function ($http, helpers, path) {
+
+    function imagesRawUrl(id) { return path.api + "/imagesRaw/" + (id || ""); }
+
+    return {
+
+        createImageRaw : function (imageData) {
+            var req = $http({
+                method: 'POST',
+                headers: helpers.getSecurityHeaders(),
+                url: imagesRawUrl(),
+                data: '"' + imageData + '"'
+            }).then(function (response) {
+                return response.data;
+            });
+            return req;
+        },
+
+        deleteImageRaw: function (fileName) {
+            var req = $http({
+                method: 'DELETE',
+                headers: helpers.getSecurityHeaders(),
+                url: imagesRawUrl(fileName)
+            }).then(function (response) {
+                return response.data;
+            });
+            return req;
+        }
+    }
+}]);
+}());
